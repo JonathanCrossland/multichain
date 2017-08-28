1b298cf45a42779695f4ceba7adfb38123d11122
Jonathan <jonathan@lucidocean.co.za>
2017-08-28 11:07:29 +0200

    useless message

--- MultiChain.Tests/UnitTest1.cs
+++ MultiChain.Tests/UnitTest1.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace MultiChain.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ConnectionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Connection()
         {
         }
     }
