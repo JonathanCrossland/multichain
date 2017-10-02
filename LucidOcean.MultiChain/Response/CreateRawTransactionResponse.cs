763d6d454fd9c8a6c6be1ef5fcac4a9beb155a1a
Dmitry Turin <turin.dmytro@gmail.com>
2017-10-02 17:48:24 +0300

    Initial implementation of commands: `createrawsendfrom`, `signrawtransaction`, `sendrawtransaction`

---
+++ LucidOcean.MultiChain/Response/CreateRawTransactionResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace LucidOcean.MultiChain.Response
+{
+    public class CreateRawTransactionResponse
+    {
+        public JsonRpcResponse<string> TxId { get; internal set; }
+
+        public JsonRpcResponse<string> TxHex { get; internal set; }
+
+        public JsonRpcResponse<SignedTransactionResponse> SignedTransaction { get; internal set; }
+    }
+}
